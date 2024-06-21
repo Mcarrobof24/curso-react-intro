e53893b51f6f7ca07bcb2b3c833cd4cfc212da9f
mcarrobo24 <mcaf.dev24@gmail.com>
2024-06-21 00:05:09 -0500

    Created components the ToDo List

---
+++ src/TodoItem.js
@@ -0,0 +1,10 @@
+function TodoItem(){
+    return (
+      <li>
+        <span>V</span>
+        <p>APRENDER REACT CON MIS ACCOUNTABILITY</p>
+        <span>X</span>
+      </li>
+    );
+}
+export{ TodoItem }
