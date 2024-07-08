7b39d0c9e3777d4da07dc77dff821e08d754c8db
mcarrobo24 <mcaf.dev24@gmail.com>
2024-07-07 20:26:46 -0500

    Creando Efectos para el loading

--- src/App/index.js
+++ src/App/index.js
@@ -16,7 +16,12 @@ import { useLocalStorage } from "./useLocalStorage";
 
 function App() {
 
-  const [todos, saveTodos] = useLocalStorage('TODOS_V1', []); 
+  const {
+    item: todos, 
+    saveItem: saveTodos,
+    loading,
+    error,
+  } = useLocalStorage('TODOS_V1', []); 
 
   const [searchValue, setSearchValue] = React.useState('');
 
@@ -51,6 +56,8 @@ function App() {
 
   return(
     < AppUI
+      loading={loading}
+      error={error}
       completedTodos ={completedTodos}
       totalTodos ={totalTodos}
       searchValue = {searchValue}
