8030889ba3db3668a1632ce7de26033c5913b1c1
mcarrobo24 <mcaf.dev24@gmail.com>
2024-07-07 21:43:44 -0500

    Cambiando el uso de Context.Consumer y Context.Provider por useContext

--- src/TodoSearch/index.js
+++ src/TodoSearch/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import { TodoContext } from '../TodoContext';
 import '../css/TodoSearch.css';
 
-function TodoSearch({
-  searchValue,
-  setSearchValue,
-}){
+function TodoSearch(){
+
+  const {
+    searchValue,
+    setSearchValue,    
+  } = React.useContext(TodoContext)
 
    return (
      <input placeholder="Cortar cebolla" 
