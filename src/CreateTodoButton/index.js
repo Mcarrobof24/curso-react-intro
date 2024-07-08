c2c250962d0bcfa9e7294730cdbf87448c6d2c23
mcarrobo24 <mcaf.dev24@gmail.com>
2024-07-08 17:00:51 -0500

    CSS al Modal, estados abrir y cerrar del modal

--- src/CreateTodoButton/index.js
+++ src/CreateTodoButton/index.js
@@ -1,17 +1,16 @@
-import '../css/CreateTodoButton.css' 
-function CreateTodoButton(){
-    return(
-        <button  className="CreateTodoButton" 
-        onClick={
-            (event)=>{
-                console.log('le diste click')
-                console.log(event)
-                console.log(event.target)
+import '../css/CreateTodoButton.css';
 
+function CreateTodoButton({ setOpenModal }){
+    return(
+        <button  
+            className= "CreateTodoButton" 
+            onClick={
+                ()=>{
+                    setOpenModal(state => !state);
+                }      
             }
-                
-        }>+</button>
+        >+</button>
     );
 }
 
-export {CreateTodoButton};
+export { CreateTodoButton };
--- src/TodosLoading/index.js
+++ src/TodosLoading/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import '../css/TodosLoading.css'
+import '../css/TodosLoading.css';
 
 function TodosLoading(){
     return(
